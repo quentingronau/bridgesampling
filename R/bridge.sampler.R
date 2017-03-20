af5eb0c27f6191043d6c90df2ab3aaee8407f114
quentingronau <quentin.f.gronau@gmail.com>
2017-03-20 11:32:22 +0100

    first commit

---
+++ R/bridge.sampler.R
@@ -0,0 +1,385 @@
+# library(mvtnorm)
+# library(Matrix)
+# library(Brobdingnag)
+# library(stringr)
+# library(coda)
+
+#' @importFrom  stats qnorm
+.transform2Real <- function(theta, lb, ub) {
+
+  ### transform samples to real line
+
+  theta_t <- theta
+  transTypes <- character()
+  cn <- colnames(theta)
+
+  for (i in seq_len(ncol(theta))) {
+
+    p <- cn[i]
+
+    if (lb[[p]] < ub[[p]] && is.infinite(lb[[p]]) && is.infinite(ub[[p]])) {
+      transTypes[[p]] <- "unbounded"
+      theta_t[,i] <- theta[,i]
+    } else if (lb[[p]] < ub[[p]] && is.finite(lb[[p]]) && is.infinite(ub[[p]])) {
+      transTypes[[p]] <- "lower-bounded"
+      theta_t[,i] <- log(theta[,i] - lb[[p]])
+    } else if (lb[[p]] < ub[[p]] && is.infinite(lb[[p]]) && is.finite(ub[[p]])) {
+      transTypes[[p]] <- "upper-bounded"
+      theta_t[,i] <- log(ub[[p]] - theta[,i])
+    } else if (lb[[p]] < ub[[p]] && is.finite(lb[[p]]) && is.finite(ub[[p]])) {
+      transTypes[[p]] <- "double-bounded"
+      theta_t[,i] <- qnorm( (theta[,i] - lb[[p]])/(ub[[p]] - lb[[p]]) )
+    } else {
+      stop("Could not transform parameters, possibly due to invalid lower and/or upper
+           prior bounds.")
+    }
+
+  }
+
+  colnames(theta_t) <- paste0("trans_", colnames(theta))
+
+  return(list(theta_t = theta_t, transTypes = transTypes))
+
+}
+
+#' @importFrom  stats pnorm
+.invTransform2Real <- function(theta_t, lb, ub) {
+
+  ### transform transformed samples back to original scales
+
+  theta <- theta_t
+  colnames(theta) <- stringr::str_sub(colnames(theta), 7)
+  cn <- colnames(theta)
+
+  for (i in seq_len(ncol(theta_t))) {
+
+    p <- cn[i]
+
+    if (lb[[p]] < ub[[p]] && is.infinite(lb[[p]]) && is.infinite(ub[[p]])) {
+      theta[,i] <- theta_t[,i]
+    } else if (lb[[p]] < ub[[p]] && is.finite(lb[[p]]) && is.infinite(ub[[p]])) {
+      theta[,i] <- exp(theta_t[,i]) + lb[[p]]
+    } else if (lb[[p]] < ub[[p]] && is.infinite(lb[[p]]) && is.finite(ub[[p]])) {
+      theta[,i] <- ub[[p]] - exp(theta_t[,i])
+    } else if (lb[[p]] < ub[[p]] && is.finite(lb[[p]]) && is.finite(ub[[p]])) {
+      theta[,i] <- pnorm(theta_t[,i])*(ub[[p]] - lb[[p]]) + lb[[p]]
+    } else {
+      stop("Could not transform parameters, possibly due to invalid lower and/or upper
+           prior bounds.")
+    }
+
+  }
+
+  return(theta)
+
+}
+
+#' @importFrom  stats dnorm
+.logJacobian <- function(theta_t, transTypes, lb, ub) {
+
+  ### compute log of Jacobian
+
+  logJ <- matrix(nrow = nrow(theta_t), ncol = ncol(theta_t))
+  cn <- stringr::str_sub(colnames(theta_t), 7)
+
+  for (i in seq_len( ncol(theta_t) )) {
+
+    p <- cn[i]
+
+    if (transTypes[[p]] == "unbounded") {
+      logJ[,i] <- 0
+    } else if (transTypes[[p]] == "lower-bounded") {
+      logJ[,i] <- theta_t[,i]
+    } else if (transTypes[[p]] == "upper-bounded") {
+      logJ[,i] <- theta_t[,i]
+    } else if (transTypes[[p]] == "double-bounded") {
+      logJ[,i] <- log(ub[[p]] - lb[[p]]) + dnorm(theta_t[,i], log = TRUE)
+    }
+
+  }
+
+  return(.rowSums(logJ, m = nrow(logJ), n = ncol(logJ)))
+
+}
+
+#' @importFrom stats median
+.run.iterative.scheme <- function(q11, q12, q21, q22, r0, tol, L, method) {
+
+  ### run iterative updating scheme (using "optimal" bridge function,
+  ### see Meng & Wong, 1996)
+
+  if (method == "normal") {
+    l1 <- q11 - q12 # log(l)
+    l2 <- q21 - q22 # log(ltilde)
+  } else if (method == "warp3") {
+    l1 <- -log(2) + log(det(L)) + (q11 - q12) # log(l)
+    l2 <-  -log(2) + log(det(L)) + (q21 - q22) # log(ltilde)
+  }
+
+  lstar <- median(l1)
+  n.1 <- length(l1)
+  n.2 <- length(l2)
+  s1 <- n.1/(n.1 + n.2)
+  s2 <- n.2/(n.1 + n.2)
+  rold <- -100
+  r <- r0
+  i <- 1
+
+  e <- as.brob( exp(1) )
+
+  while (abs((r - rold)/r) > tol) {
+
+    cat(paste0("Iterative scheme iteration: ", i, "\n"))
+    rold <- r
+    numi <- as.numeric( e^(l2 - lstar)/(s1 * e^(l2 - lstar) + s2 *  r) )
+    deni <- as.numeric( 1/(s1 * e^(l1 - lstar) + s2 * r) )
+    r <- (n.1/n.2) * sum(numi)/sum(deni)
+    i <- i + 1
+
+  }
+
+  logml <- log(r) + lstar
+  return(list(logml = logml, niter = i-1))
+
+}
+
+#' @importFrom mvtnorm rmvnorm dmvnorm
+#' @importFrom Matrix nearPD
+#' @import Brobdingnag
+#' @import stringr
+#' @import coda
+#' @importFrom snow setDefaultClusterOptions
+#' @import snowfall
+#' @import rlecuyer
+#' @importFrom Rcpp sourceCpp
+#' @import stats
+.bridge.sampler.normal <- function(post.samples, log.posterior, data, lb, ub,
+                                  cores, packages, rcppFile, r0, tol) {
+
+  # transform parameters to real line
+  tmp <- .transform2Real(post.samples, lb, ub)
+  theta_t <- tmp$theta_t
+  transTypes <- tmp$transTypes
+
+  # split samples for proposal/iterative scheme
+  nr <- nrow(post.samples)
+  samples4fit_index <- seq_len(nr) %% 2 == TRUE # split samples in even/odd
+  samples_4_fit <- theta_t[samples4fit_index, ,drop = FALSE]
+  samples_4_iter <- theta_t[!samples4fit_index, , drop = FALSE]
+  n_post <- nrow(samples_4_iter)
+
+  # get mean & covariance matrix and generate samples from proposal
+  m <- apply(samples_4_fit, 2, mean)
+  V_tmp <- cov(samples_4_fit)
+  V <- as.matrix(nearPD(V_tmp)$mat) # make sure that V is positive-definite
+  gen_samples <- rmvnorm(n_post, mean = m, sigma = V)
+  colnames(gen_samples) <- colnames(samples_4_iter)
+
+  # evaluate multivariate normal distribution for posterior samples and generated samples
+  q12 <- dmvnorm(samples_4_iter, mean = m, sigma = V, log = TRUE)
+  q22 <- dmvnorm(gen_samples, mean = m, sigma = V, log = TRUE)
+
+  # evaluate log likelihood times prior for posterior samples and generated samples
+  if (cores == 1) {
+
+    q11 <- apply(.invTransform2Real(samples_4_iter, lb, ub), 1, log.posterior,
+                 data = data) + .logJacobian(samples_4_iter, transTypes, lb, ub)
+    q21 <- apply(.invTransform2Real(gen_samples, lb, ub), 1, log.posterior,
+                 data = data) + .logJacobian(gen_samples, transTypes, lb, ub)
+
+  } else if (cores > 1) {
+
+    # require(snowfall, quietly = TRUE)
+    # require(rlecuyer, quietly = TRUE)
+    sfInit(parallel = TRUE, cpus = cores, type = "SOCK")
+    sfClusterSetupRNG()
+    sapply(packages, function(x) sfLibrary(x, character.only = TRUE))
+    sfExport("rcppFile")
+    sfExportAll()
+
+    if ( ! is.null(rcppFile)) {
+      sfLibrary("Rcpp", character.only = TRUE)
+      sfClusterEval(sourceCpp(file = rcppFile))
+    }
+
+    q11 <- sfApply(.invTransform2Real(samples_4_iter, lb, ub), 1, log.posterior,
+                   data = data) + .logJacobian(samples_4_iter, transTypes, lb, ub)
+    q21 <- sfApply(.invTransform2Real(gen_samples, lb, ub), 1, log.posterior,
+                   data = data) + .logJacobian(gen_samples, transTypes, lb, ub)
+
+    sfStop()
+
+  }
+
+  # run iterative updating scheme to compute log of marginal likelihood
+  tmp <- .run.iterative.scheme(q11 = q11, q12 = q12, q21 = q21, q22 = q22,
+                              r0 = r0, tol = tol, L = NULL, method = "normal")
+  logml <- tmp$logml
+  niter <- tmp$niter
+
+  out <- list(logml = logml, niter = niter, method = "normal", q11 = q11,
+              q12 = q12, q21 = q21, q22 = q22)
+
+  return(out)
+
+}
+
+#' @importFrom mvtnorm rmvnorm dmvnorm
+#' @importFrom Matrix nearPD
+#' @import Brobdingnag
+#' @import stringr
+#' @import coda
+#' @importFrom snow setDefaultClusterOptions
+#' @import snowfall
+#' @import rlecuyer
+#' @importFrom Rcpp sourceCpp
+#' @import stats
+.bridge.sampler.warp3 <- function(post.samples, log.posterior, data, lb, ub,
+                                 cores, packages, rcppFile, r0, tol) {
+
+  # transform parameters to real line
+  tmp <- .transform2Real(post.samples, lb, ub)
+  theta_t <- tmp$theta_t
+  transTypes <- tmp$transTypes
+
+  # split samples for proposal/iterative scheme
+  nr <- nrow(post.samples)
+  samples4fit_index <- seq_len(nr) %% 2 == TRUE # split samples in even/odd
+  samples_4_fit <- theta_t[samples4fit_index, ,drop = FALSE]
+  samples_4_iter <- theta_t[!samples4fit_index, , drop = FALSE]
+  n_post <- nrow(samples_4_iter)
+
+  # get mean & covariance matrix and generate samples from proposal
+  m <- apply(samples_4_fit, 2, mean)
+  V_tmp <- cov(samples_4_fit)
+  V <- as.matrix(nearPD(V_tmp)$mat) # make sure that V is positive-definite
+  L <- t(chol(V))
+  gen_samples <- rmvnorm(n_post, sigma = diag(ncol(samples_4_fit)))
+  colnames(gen_samples) <- colnames(samples_4_iter)
+
+  # evaluate multivariate normal distribution for posterior samples and generated samples
+  q12 <- dmvnorm((samples_4_iter - matrix(m, nrow = n_post, ncol = length(m), byrow = TRUE)) %*%
+                   t(solve(L)), sigma = diag(ncol(samples_4_fit)), log = TRUE)
+  q22 <- dmvnorm(gen_samples, sigma = diag(ncol(samples_4_fit)), log = TRUE)
+
+  e <- as.brob( exp(1) )
+
+  # evaluate log likelihood times prior for posterior samples and generated samples
+  if (cores == 1) {
+
+    q11 <- log(e^(apply(.invTransform2Real(samples_4_iter, lb, ub), 1, log.posterior,
+                        data = data) + .logJacobian(samples_4_iter, transTypes, lb, ub)) +
+                 e^(apply(.invTransform2Real(matrix(2*m, nrow = n_post, ncol = length(m), byrow = TRUE) -
+                                              samples_4_iter, lb, ub), 1, log.posterior, data = data) +
+                      .logJacobian(matrix(2*m, nrow = n_post, ncol = length(m), byrow = TRUE) -
+                                    samples_4_iter, transTypes, lb, ub)))
+    q21 <- log(e^(apply(.invTransform2Real(matrix(m, nrow = n_post, ncol = length(m), byrow = TRUE) -
+                                            gen_samples %*% t(L), lb, ub), 1, log.posterior, data = data) +
+                    .logJacobian(matrix(m, nrow = n_post, ncol = length(m), byrow = TRUE) -
+                                  gen_samples %*% t(L), transTypes, lb, ub)) +
+                 e^(apply(.invTransform2Real(matrix(m, nrow = n_post, ncol = length(m), byrow = TRUE) +
+                                              gen_samples %*% t(L), lb, ub), 1, log.posterior, data = data) +
+                      .logJacobian(matrix(m, nrow = n_post, ncol = length(m), byrow = TRUE) +
+                                    gen_samples %*% t(L), transTypes, lb, ub)))
+
+  } else if (cores > 1) {
+
+    # require(snowfall, quietly = TRUE)
+    # require(rlecuyer, quietly = TRUE)
+    sfInit(parallel = TRUE, cpus = cores, type = "SOCK")
+    sfClusterSetupRNG()
+    sapply(packages, function(x) sfLibrary(x, character.only = TRUE))
+    sfExport("rcppFile")
+    sfExportAll()
+
+    if ( ! is.null(rcppFile)) {
+      sfLibrary("Rcpp", character.only = TRUE)
+      sfClusterEval(sourceCpp(file = rcppFile))
+    }
+
+    q11 <- log(e^(sfApply(.invTransform2Real(samples_4_iter, lb, ub), 1, log.posterior,
+                          data = data) + .logJacobian(samples_4_iter, transTypes, lb, ub)) +
+                 e^(sfApply(.invTransform2Real(matrix(2*m, nrow = n_post, ncol = length(m), byrow = TRUE) -
+                                                samples_4_iter, lb, ub), 1, log.posterior, data = data) +
+                      .logJacobian(matrix(2*m, nrow = n_post, ncol = length(m), byrow = TRUE) -
+                                    samples_4_iter, transTypes, lb, ub)))
+    q21 <- log(e^(sfApply(.invTransform2Real(matrix(m, nrow = n_post, ncol = length(m), byrow = TRUE) -
+                                              gen_samples %*% t(L), lb, ub), 1, log.posterior, data = data) +
+                    .logJacobian(matrix(m, nrow = n_post, ncol = length(m), byrow = TRUE) -
+                                  gen_samples %*% t(L), transTypes, lb, ub)) +
+                 e^(sfApply(.invTransform2Real(matrix(m, nrow = n_post, ncol = length(m), byrow = TRUE) +
+                                                gen_samples %*% t(L), lb, ub), 1, log.posterior, data = data) +
+                      .logJacobian(matrix(m, nrow = n_post, ncol = length(m), byrow = TRUE) +
+                                    gen_samples %*% t(L), transTypes, lb, ub)))
+
+    sfStop()
+
+  }
+
+  # run iterative updating scheme to compute log of marginal likelihood
+  tmp <- .run.iterative.scheme(q11 = q11, q12 = q12, q21 = q21, q22 = q22,
+                              r0 = r0, tol = tol, L = L, method = "warp3")
+  logml <- tmp$logml
+  niter <- tmp$niter
+
+  out <- list(logml = logml, niter = niter, method = "warp3", q11 = q11,
+              q12 = q12, q21 = q21, q22 = q22)
+
+  return(out)
+
+}
+
+#' Computes log marginal likelihood via bridge sampling
+#' @export
+#' @title Computes log marginal likelihood via bridge sampling
+#' @name bridge.sampler
+#' @param post.samples matrix with posterior samples (colnames need to correspond to parameter names)
+#' @param log.posterior function for computing log of prior times likelihood
+#' @param data data
+#' @param lb named vector with lower bounds for parameters
+#' @param ub named vector with upper bounds for parameters
+#' @param method either "normal" or "warp3"
+#' @param cores number of cores used for evaluating log.posterior
+#' @param packages character vector with names of packages needed for evaluating log.posterior in parallel (only relevant if cores > 1)
+#' @param rcppFile in case cores > 1 and log.posterior is an Rcpp function, rcppFile specifies the path to the cpp file (needs to be compiled on all cores)
+#' @details Bridge sampling is implemented as described in Meng and Wong (1996, see equation 4.1) using the "optimal" bridge function. When \code{method = "normal"}, the proposal distribution is a multivariate normal distribution with mean vector equal to the column means of \code{post.samples} and covariance matrix equal to the sample covariance matrix of \code{post.samples}. When \code{method = "warp3"},
+#' the proposal distribution is a standard multivariate normal distribution and the posterior distribution is "warped" (Meng & Schilling, 2002) so that it has the same mean vector, covariance matrix and skew as post.samples. \code{method = "warp3"} usually yields more precise results, but it takes approximately twice as long as \code{method = "normal"}.
+#' @return a list of the class \code{bridge} with the objects:
+#' \itemize{
+#'  \item \code{logml}: estimate of log marginal likelihood
+#'  \item \code{niter}: number of iterations of the iterative updating scheme
+#'  \item \code{method}: bridge sampling method that was used to obtain estimate
+#'  \item \code{q11}: log.posterior evaluations for posterior samples
+#'  \item \code{q12}: log proposal evaluations for posterior samples
+#'  \item \code{q21}: log.posterior evaluations for samples from proposal
+#'  \item \code{q22}: log proposal evaluations for samples from proposal
+#' }
+#' @author Quentin F. Gronau
+#' @references
+#' Meng, X.-L., & Wong, W. H. (1996). Simulating ratios of normalizing constants via a simple identity: A theoretical exploration. Statistica Sinica, 6, 831-860.
+#'
+#' Meng, X.-L., & Schilling, S. (2002). Warp bridge sampling. Journal of Computational and Graphical Statistics, 11(3), 552-586.
+bridge.sampler <- function(post.samples = NULL, log.posterior = NULL, data = NULL,
+                           lb = NULL, ub = NULL, method = "normal", cores = 1,
+                           packages = NULL, rcppFile = NULL) {
+
+  # see Meng & Wong (1996), equation 4.1
+
+  out <- do.call(what = paste0(".bridge.sampler.", method),
+          args = list(post.samples = post.samples, log.posterior = log.posterior,
+                      data = data, lb = lb, ub = ub, cores = cores,
+                      packages = packages, rcppFile = rcppFile, r0 = 0,
+                      tol = 1e-10))
+  class(out) <- "bridge"
+  return(out)
+
+}
+
+print.bridge <- function(x, ...) {
+
+  cat("Bridge sampling estimate of the log marginal likelihood: ",
+      round(x$logml, 3), ". \nEstimate obtained in ", x$niter,
+      " iterations via method \"", x$method, "\".", sep = "")
+}
+
+
---
+++ R/error.measures.R
@@ -0,0 +1,57 @@
+#' Computes error measures for estimated marginal likelihood
+#' @export
+#' @title Computes error measures for estimated marginal likelihood
+#' @name error.measures
+#' @param bridgeObject output from \code{\link{bridge.sampler}}
+#' @details Computes approximate error measures for marginal likelihood bridge sampling estimates
+#' based on Fruehwirth-Schnatter (2004).
+#' @return a list with the objects:
+#' \itemize{
+#'  \item \code{re2}: approximate relative mean squared error for marginal likelihood estimate
+#'  \item \code{cv}: coefficient of variation for marginal likelihood estimate (assumes that bridge estimate is unbiased)
+#'  \item \code{percentage}: percentage error of marginal likelihood estimate
+#' }
+#' @author Quentin F. Gronau
+#' @references
+#' Frühwirth‐Schnatter, S. (2004). Estimating marginal likelihoods for mixture and Markov switching models using bridge sampling techniques. The Econometrics Journal, 7, 143-167.
+#' @import Brobdingnag
+#' @importFrom coda spectrum0.ar
+error.measures <- function(bridgeObject) {
+
+  if (bridgeObject$method == "warp3")
+    stop("error.measures not implemented for warp3 method. We recommend to run
+         the warp3 procedure multiple times to assess the uncertainty of the
+         estimate.")
+
+  e <- as.brob( exp(1) )
+
+  ml <- e^(bridgeObject$logml)
+  g_p <- e^(bridgeObject$q12)
+  g_g <- e^(bridgeObject$q22)
+  priorTimesLik_p <- e^(bridgeObject$q11)
+  priorTimesLik_g <- e^(bridgeObject$q21)
+  p_p <- priorTimesLik_p/ml
+  p_g <- priorTimesLik_g/ml
+
+  N1 <- length(p_p)
+  N2 <- length(g_g)
+  s1 <- N1/(N1 + N2)
+  s2 <- N2/(N1 + N2)
+
+  f1 <- as.numeric( p_g/(s1*p_g + s2*g_g) )
+  f2 <- as.numeric( g_p/(s1*p_p + s2*g_p) )
+  rho_f2 <- spectrum0.ar( f2 )$spec
+
+  term1 <- 1/N2 * var( f1 ) / mean( f1 )^2
+  term2 <- rho_f2/N1 * var( f2 ) / mean( f2 )^2
+
+  re2 <- term1 + term2
+
+  # convert to coefficient of variation (assumes that bridge estimate is unbiased)
+  cv <- sqrt(re2)
+
+  # convert to percentage error
+  percentage <- cv*100
+  return(list(re2 = re2, cv = cv, percentage = percentage))
+
+}
