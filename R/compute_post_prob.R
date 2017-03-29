8ceaaa996441d17c4e5e6a44f19949bc9f9df71c
singmann <singmann@gmail.com>
2017-03-30 00:14:04 +0200

    added post_prob.bridge_list method, updated examples.

--- R/compute_post_prob.R
+++ R/compute_post_prob.R
@@ -1,44 +1,74 @@
 #' Generic function that computes posterior model probabilities from marginal likelihoods
 #' @export
 #' @title Posterior Model Probabilities from Marginal Likelihoods
-#' @param x Object of class \code{"bridge"} or \code{"bridge_list"} as returned from \code{\link{bridge_sampler}}. The default method simply assumes all passed objects are numeric log marginal likelihoods (e.g., from \code{\link{logml}}).
+#' @param x Object of class \code{"bridge"} or \code{"bridge_list"} as returned from \code{\link{bridge_sampler}}. Additionally, the default method assumes that all passed objects are numeric log marginal likelihoods (e.g., from \code{\link{logml}}) and will throw an error otherwise.
 #' @param ... further objects of class \code{"bridge"} or \code{"bridge_list"} as returned from \code{\link{bridge_sampler}}. Or numeric values for the default method.
-#' @param prior_prob numeric vector with prior model probabilities. If omitted, a uniform prior is used (i.e., all models are equally likely a priori).
-#' @return Numeric vector with posterior model probabilities (with names derived from the input).
+#' @param prior_prob numeric vector with prior model probabilities. If omitted, a uniform prior is used (i.e., all models are equally likely a priori). The default \code{NULL} correspond to equal prior model weights.
+#' @param model_names If \code{NULL} (the default) will use model names derived from deparsing the call. Otherwise will use the passed values as model names.
+#'
+#' @return For the default method and the method for \code{"bridge"} objects, a named numeric vector with posterior model probabilities (i.e., which sums to one).
+#'
+#' For the method for \code{"bridge_list"} objects, a matrix consisting of posterior model probabilities where each row sums to one and gives the model probabilities for one set of logmls. The (named) columns correspond to the models and the number of rows is given by the \code{"bridge_list"} element with the most \code{repetitions}. Elements with fewer repetitions will be recycled (with warning).
 #' @author Quentin F. Gronau and Henrik Singmann
 #' @note For realistic examples, see \code{\link{bridge_sampler}} and the accompanying vignette: \cr \code{vignette("bridgesampling_example")}
 #' @example examples/example.post_prob.R
 #'
-post_prob <- function (x, ..., prior_prob) {
+post_prob <- function (x, ..., prior_prob = NULL, model_names = NULL) {
    UseMethod("post_prob", x)
 }
 
 #' @rdname post_prob
 #' @export
-post_prob.bridge <- function(x, ..., prior_prob) {
+post_prob.bridge <- function(x, ..., prior_prob = NULL, model_names = NULL) {
   dots <- list(...)
   mc <- match.call()
   modb <- (as.logical(vapply(dots, is, NA, "bridge")) |
              as.logical(vapply(dots, is, NA, "bridge_list"))
            )
-  model_names <- c(deparse(mc[["x"]]), vapply(which(modb), function(x) deparse(mc[[x+2]]), ""))
+  if (is.null(model_names))
+    model_names <- c(deparse(mc[["x"]]), vapply(which(modb), function(x) deparse(mc[[x+2]]), ""))
   if (sum(modb) != length(dots))
-    warning("Objects not of class 'bridge' or 'bridge_repetitions' are ignored.", call. = FALSE)
+    warning("Objects not of class 'bridge' or 'bridge_list' are ignored.", call. = FALSE)
 
   logml <- vapply(c(list(x), dots[modb]), logml, FUN.VALUE = 0)
 
   .post_prob_calc(logml=logml, model_names = model_names, prior_prob=prior_prob)
+}
 
+
+#' @rdname post_prob
+#' @export
+post_prob.bridge_list <- function(x, ..., prior_prob = NULL, model_names = NULL) {
+  dots <- list(...)
+  mc <- match.call()
+  modb <- (as.logical(vapply(dots, is, NA, "bridge")) |
+             as.logical(vapply(dots, is, NA, "bridge_list"))
+           )
+  #modl <- as.logical(vapply(dots, is, NA, "bridge_list"))
+  if (is.null(model_names))
+    model_names <- c(deparse(mc[["x"]]), vapply(which(modb), function(x) deparse(mc[[x+2]]), ""))
+  if (sum(modb) != length(dots))
+    warning("Objects not of class 'bridge' or 'bridge_list' are ignored.", call. = FALSE)
+
+  logml <- lapply(c(list(x), dots[modb]), "[[", i = "logml")
+  len <- vapply(logml, length, FUN.VALUE = 0)
+  if (!all(len == max(len))) {
+    warning("Not all objects provide ", max(len), " logmls. Some values are recycled.", call. = FALSE)
+    logml <- lapply(logml, function(x) rep(x, length.out = max(len)))
+  }
+  t(apply(as.matrix(as.data.frame(logml)), 1, function(x)
+    .post_prob_calc(logml=x, model_names = model_names, prior_prob=prior_prob)))
 }
 
 #' @rdname post_prob
 #' @export
-post_prob.default <- function(x, ..., prior_prob) {
+post_prob.default <- function(x, ..., prior_prob = NULL, model_names = NULL) {
   dots <- list(...)
   mc <- match.call()
-  model_names <- c(rep(deparse(mc[["x"]]), length(x)),
-                   rep(vapply(seq_along(dots), function(x) deparse(mc[[x+2]]), ""),
-                       times = vapply(dots, length, 0)))
+  if (is.null(model_names))
+    model_names <- c(rep(deparse(mc[["x"]]), length(x)),
+                     rep(vapply(seq_along(dots), function(x) deparse(mc[[x+2]]), ""),
+                         times = vapply(dots, length, 0)))
   logml <- c(x, unlist(dots))
   if (!is.numeric(logml)) {
     stop("logml values need to be numeric", call. = FALSE)
@@ -50,7 +80,7 @@ post_prob.default <- function(x, ..., prior_prob) {
 .post_prob_calc <- function(logml, model_names, prior_prob) {
   e <- as.brob(exp(1))
 
-  if(missing(prior_prob))
+  if(is.null(prior_prob))
     prior_prob <- rep(1/length(logml), length(logml))
 
   if(!isTRUE(all.equal(sum(prior_prob), 1)))
--- examples/example.post_prob.R
+++ examples/example.post_prob.R
@@ -15,5 +15,24 @@ post_prob(H0, H1, H2)
 post_prob(H0$logml, H1$logml, H2$logml)
 post_prob(c(H0$logml, H1$logml, H2$logml))
 post_prob(H0$logml, c(H1$logml, H2$logml))
+post_prob(H0$logml, c(H1$logml, H2$logml), model_names = c("H0", "H1", "H2"))
 
+
+### with bridge list elements:
+H0L <- structure(list(logml = c(-20.8088381186739, -20.8072772698116,
+-20.808454454621, -20.8083419072281, -20.8087870541247, -20.8084887398113,
+-20.8086023582344, -20.8079083169745, -20.8083048489095, -20.8090050811436
+), niter = c(4, 4, 4, 4, 4, 4, 4, 4, 4, 4), method = "normal",
+    repetitions = 10), .Names = c("logml", "niter", "method",
+"repetitions"), class = "bridge_list")
+
+H1L <- structure(list(logml = c(-17.961665507006, -17.9611290723151,
+-17.9607509604499, -17.9608629535992, -17.9602093576442, -17.9600223300432,
+-17.9610157118017, -17.9615557696561, -17.9608437034849, -17.9606743200309
+), niter = c(4, 4, 4, 4, 4, 4, 4, 4, 3, 4), method = "normal",
+    repetitions = 10), .Names = c("logml", "niter", "method",
+"repetitions"), class = "bridge_list")
+
+post_prob(H1L, H0L)
+post_prob(H1L, H0L, H0) # last element recycled with warning.
 
