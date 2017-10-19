ab3d7d035779e60b72418728194fd06d8706600b
singmann <singmann@gmail.com>
2017-10-19 11:06:23 +0200

    added revdep_checks

--- development.R
+++ development.R
@@ -8,3 +8,13 @@ test()
 roxygen2::roxygenize()
 
 build_vignettes()
+
+### check reverse dependencies:
+
+#revdep()
+devtools::revdep_check(libpath = "../revdep", check_dir = "../revdep_checks")
+#devtools::install.packages("brms", lib = "../revdep")
+devtools::revdep_check_resume()
+devtools::revdep_check_save_summary()
+devtools::revdep_check_print_problems()
+devtools::revdep_maintainers()
