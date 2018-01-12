8d3c42fa6f5fdae53a66009151181ec7a8de6dda
Henrik Singmann <singmann@gmail.com>
2018-01-12 17:46:18 +0100

    moved stanfit helper function to bridge_sampler_tools.R

--- R/bridge_sampler.R
+++ R/bridge_sampler.R
@@ -463,30 +463,3 @@ bridge_sampler.MCMC_refClass <- function(samples,
 
 }
 
-######### tools for stanfit method ########
-# taken from rstan:
-.rstan_relist <- function (x, skeleton) {
-  lst <- utils::relist(x, skeleton)
-  for (i in seq_along(skeleton)) dim(lst[[i]]) <- dim(skeleton[[i]])
-  lst
-}
-
-# taken from rstan:
-.create_skeleton <- function (pars, dims) {
-  lst <- lapply(seq_along(pars), function(i) {
-    len_dims <- length(dims[[i]])
-    if (len_dims < 1)
-      return(0)
-    return(array(0, dim = dims[[i]]))
-  })
-  names(lst) <- pars
-  lst
-}
-
-.stan_log_posterior <- function(s.row, data) {
-  out <- tryCatch(rstan::log_prob(object = data$stanfit, upars = s.row), error = function(e) -Inf)
-  if (is.na(out)) out <- -Inf
-  return(out)
-}
-
-
--- R/bridge_sampler_tools.R
+++ R/bridge_sampler_tools.R
@@ -1,5 +1,37 @@
 
 #--------------------------------------------------------------------------
+# functions for Stan support via rstan
+#--------------------------------------------------------------------------
+
+# taken from rstan:
+.rstan_relist <- function (x, skeleton) {
+  lst <- utils::relist(x, skeleton)
+  for (i in seq_along(skeleton)) dim(lst[[i]]) <- dim(skeleton[[i]])
+  lst
+}
+
+# taken from rstan:
+.create_skeleton <- function (pars, dims) {
+  lst <- lapply(seq_along(pars), function(i) {
+    len_dims <- length(dims[[i]])
+    if (len_dims < 1)
+      return(0)
+    return(array(0, dim = dims[[i]]))
+  })
+  names(lst) <- pars
+  lst
+}
+
+.stan_log_posterior <- function(s.row, data) {
+  out <- tryCatch(rstan::log_prob(object = data$stanfit, upars = s.row), error = function(e) -Inf)
+  if (is.na(out)) out <- -Inf
+  return(out)
+}
+
+
+
+
+#--------------------------------------------------------------------------
 # functions for nimble support
 #--------------------------------------------------------------------------
 
