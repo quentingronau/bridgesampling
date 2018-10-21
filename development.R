3eb28b5232cba2e588415842881ab82ae2de6b3b
Henrik Singmann <singmann@gmail.com>
2018-10-21 15:23:18 +0200

    small changes before submitting to CRAN

--- development.R
+++ development.R
@@ -8,7 +8,7 @@ devtools::test()
 roxygen2::roxygenize()
 
 build_vignettes()
-devtools::build()
+devtools::build(args = '--compact-vignettes="gs+qpdf"')
 
 Sys.setenv(`_R_CHECK_FORCE_SUGGESTS_` = "false")
 devtools::check()
