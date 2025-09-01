58670610f91ffc8efa96fe341d327873fbd611e2
Henrik Singmann <singmann@gmail.com>
2025-09-01 15:25:54 +0100

    UTF-8 not needed after removing 1 non-ASCII char

--- R/bridge_sampler.R
+++ R/bridge_sampler.R
@@ -26,9 +26,10 @@
 #'@param ub named vector with upper bounds for parameters.
 #'@param repetitions number of repetitions.
 #'@param method either \code{"normal"} or \code{"warp3"}.
-#'@param use_ess Logical. If \code{TRUE}, the iterative scheme’s uncertainty
-#'   calculations replace the nominal sample size with the effective sample size (ESS),
-#'   making MCSE computation take into account autocorrelation in MCMC samples.
+#'@param use_ess Logical. If \code{TRUE}, the iterative scheme's uncertainty
+#'  calculations replace the nominal sample size with the effective sample size
+#'  (ESS), making MCSE computation take into account autocorrelation in MCMC
+#'  samples.
 #'@param cores number of cores used for evaluating \code{log_posterior}. On
 #'  unix-like systems (where \code{.Platform$OS.type == "unix"} evaluates to
 #'  \code{TRUE}; e.g., Linux and Mac OS) forking via \code{\link{mclapply}} is
